62b67e9b08e925ebbb2653e9f05519324a230d30
lin-zone <z_one10@163.com>
2019-12-23 01:37:00 +0800

    add and div func

--- tests/test_test_travis.py
+++ tests/test_test_travis.py
@@ -1,5 +1,15 @@
 from test_travis import __version__
+from test_travis.main import add, div
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_add():
+    assert 3 == add(1, 2)
+
+def test_div():
+    assert 2 == div(6, 3)
+
+def test_div_zero():
+    assert 1 == div(1, 0)
